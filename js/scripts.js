bb550ae4f82570ac38a39fcef030107c1c40dc38
ms-sdev <stella.cherotich@moringaschool.com>
2025-01-13 16:06:35 +0300

    add working project

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,14 @@
-function Contact(firstName, lastName, phoneNumber) {
+// Business Logic for AddressBook ---------
+function AddressBook() {
+    this.contacts = {};
+  }
+  
+  AddressBook.prototype.addContact = function(contact) {
+    this.contacts[contact.firstName] = contact;
+  };
+  
+  // Business Logic for Contacts ---------
+  function Contact(firstName, lastName, phoneNumber) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.phoneNumber = phoneNumber;
